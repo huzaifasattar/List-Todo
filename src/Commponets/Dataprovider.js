c4ee09a96112da239f8bbe702176ede8ecd83a55
Huzaifa Sattar <huzaifasattar@Macbooks-MacBook-Pro.local>
2023-01-22 18:40:18 +0500

    first commit

--- src/Commponets/Dataprovider.js
+++ src/Commponets/Dataprovider.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import { createContext } from 'react'
+
 export const DataContex = createContext();
+
 export const Dataprovider = (props) => {
     
-        const itemStore = JSON.parse(localStorage.getItem('itemStore'))
+    const itemStore = JSON.parse(localStorage.getItem('itemStore'))
        
     const [todos, setTodos] = useState(itemStore)
+    
     useEffect(() => {
         localStorage.setItem('itemStore',JSON.stringify(todos))
     }, [todos])
-  return (
+  
+    return (
       <DataContex.Provider value={[todos,setTodos]}>{props.children}</DataContex.Provider>
   )
 }
